965b430cefc5e8509557b7049e9a3817189f90a1
Yorda <jf.tomasi@gmail.com>
2012-09-04 14:56:13 +0200

    Add classes for get the syndication url in a web page

---
+++ src/test/java/all/utility/WebSiteUtilTest.java
@@ -0,0 +1,31 @@
+package all.utility;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+public class WebSiteUtilTest {
+
+	
+	@Test
+	public void testSearchSyndicate() {
+		String syndicate = null;
+		
+		try {
+			syndicate = WebSiteUtil.searchSyndicate("http://linuxfr.org/");
+		} catch (IOException ioe) {
+			fail("Unable to connect to web site");
+		}
+		assertEquals("http://linuxfr.org/news.atom", syndicate);
+		
+		try {
+			syndicate = WebSiteUtil.searchSyndicate("http://www.pcinpact.com/");
+		} catch (IOException ioe) {
+			fail("Unable to connect to web site");
+		}
+		assertEquals("http://www.pcinpact.com/rss/news.xml", syndicate);
+	}
+
+}
