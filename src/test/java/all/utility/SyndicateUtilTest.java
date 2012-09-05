c165d13242218aaf158eea1633bbd012808d04b9
Yorda <jf.tomasi@gmail.com>
2012-09-05 09:47:21 +0200

    Add test for syndicate utility

---
+++ src/test/java/all/utility/SyndicateUtilTest.java
@@ -0,0 +1,40 @@
+package all.utility;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+
+import org.junit.Test;
+
+import com.sun.syndication.feed.synd.SyndEntry;
+import com.sun.syndication.io.FeedException;
+
+public class SyndicateUtilTest {
+
+	@Test
+	public void testLastEntries() {
+
+		URL url = null;
+		try {
+			url = new URL("http://www.pcinpact.com/rss/news.xml");
+		} catch (MalformedURLException mue) {
+			fail("Bad syndication url");
+		}
+
+		List<SyndEntry> entries = null;
+		try {
+			entries = SyndicateUtil.lastEntries(url);
+		} catch (IOException e) {
+			fail("Unable to get feeds");
+		} catch (FeedException e) {
+			fail("Bad feeds returned by site");
+		}
+
+		assertNotNull(entries);
+	}
+
+}
